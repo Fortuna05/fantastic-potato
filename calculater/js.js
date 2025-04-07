fa20eaa317035f3ed6c73f26ad71b950a675b707
Murid <muridinabdulhamidov@gmail.com>
2025-04-07 21:34:38 +0300

    чуть изменил

--- calculater/js.js
+++ calculater/js.js
@@ -1,54 +1,57 @@
 let numscreen = document.getElementById('numscreen')
 
-                    const num_ps1 = document.getElementById('one')
-                    const num_ps2 = document.getElementById('two')
-                    const num_ps3 = document.getElementById('three')
-                    const num_ps4 = document.getElementById('four')
-                    const num_ps5 = document.getElementById('five')
-                    const num_ps6 = document.getElementById('six')
-                    const num_ps7 = document.getElementById('seven')
-                    const num_ps8 = document.getElementById('eight')
-                    const num_ps9 = document.getElementById('nine')
-                    const num_ps0 = document.getElementById('zero')
+const num_ps1 = document.getElementById('one')
+const num_ps2 = document.getElementById('two')
+const num_ps3 = document.getElementById('three')
+const num_ps4 = document.getElementById('four')
+const num_ps5 = document.getElementById('five')
+const num_ps6 = document.getElementById('six')
+const num_ps7 = document.getElementById('seven')
+const num_ps8 = document.getElementById('eight')
+const num_ps9 = document.getElementById('nine')
+const num_ps0 = document.getElementById('zero')
 
-                    num_ps1.addEventListener('click', function(){
-                        const textNode = document.createTextNode('1')
-                        numscreen.appendChild(textNode)
-                    })
-                    num_ps2.addEventListener('click', function(){
-                        const textNode = document.createTextNode('2')
-                        numscreen.appendChild(textNode)
-                    })
-                    num_ps3.addEventListener('click', function(){
-                        const textNode = document.createTextNode('3')
-                        numscreen.appendChild(textNode)
-                    })
-                    num_ps4.addEventListener('click', function(){
-                        const textNode = document.createTextNode('4')
-                        numscreen.appendChild(textNode)
-                    })
-                    num_ps5.addEventListener('click', function(){
-                        const textNode = document.createTextNode('5')
-                        numscreen.appendChild(textNode)
-                    })
-                    num_ps6.addEventListener('click', function(){
-                        const textNode = document.createTextNode('6')
-                        numscreen.appendChild(textNode)
-                    })
-                    num_ps7.addEventListener('click', function(){
-                        const textNode = document.createTextNode('7')
-                        numscreen.appendChild(textNode)
-                    })
-                    num_ps8.addEventListener('click', function(){
-                        const textNode = document.createTextNode('8')
-                        numscreen.appendChild(textNode)
-                    })
-                    num_ps9.addEventListener('click', function(){
-                        const textNode = document.createTextNode('9')
-                        numscreen.appendChild(textNode)
-                    })
-                    num_ps0.addEventListener('click', function(){
-                        const textNode = document.createTextNode('0')
-                        numscreen.appendChild(textNode)
-                    })
+num_ps1.addEventListener('click', function () {
+    const textNode = document.createTextNode('1')
+    numscreen.appendChild(textNode)
+})
+num_ps2.addEventListener('click', function () {
+    const textNode = document.createTextNode('2')
+    numscreen.appendChild(textNode)
+})
+num_ps3.addEventListener('click', function () {
+    const textNode = document.createTextNode('3')
+    numscreen.appendChild(textNode)
+})
+num_ps4.addEventListener('click', function () {
+    const textNode = document.createTextNode('4')
+    numscreen.appendChild(textNode)
+})
+num_ps5.addEventListener('click', function () {
+    const textNode = document.createTextNode('5')
+    numscreen.appendChild(textNode)
+})
+num_ps6.addEventListener('click', function () {
+    const textNode = document.createTextNode('6')
+    numscreen.appendChild(textNode)
+})
+num_ps7.addEventListener('click', function () {
+    const textNode = document.createTextNode('7')
+    numscreen.appendChild(textNode)
+})
+num_ps8.addEventListener('click', function () {
+    const textNode = document.createTextNode('8')
+    numscreen.appendChild(textNode)
+})
+num_ps9.addEventListener('click', function () {
+    const textNode = document.createTextNode('9')
+    numscreen.appendChild(textNode)
+})
+num_ps0.addEventListener('click', function () {
+    const textNode = document.createTextNode('0')
+    numscreen.appendChild(textNode)
+})
+
+
+
 
